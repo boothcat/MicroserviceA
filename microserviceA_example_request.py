7dae2f7762d06c7527cf0cf5f08aaf8f808cf124
Catherine Booth <97126252+boothcat@users.noreply.github.com>
2025-05-19 20:09:46 -0400

    Added microserviceA_example_request.py

--- microserviceA_example_request.py
+++ microserviceA_example_request.py
@@ -7,11 +7,11 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5985")
 
 # Data to send to microservice
-# sort_column = column name from csv file to sort by
+# sort_column = column name from csv file to sort by ("Title", "Year", "Format")
 # path = absolute path to csv file
 # sort_order = "asc" for ascending order or "desc" for descending order
 movie_vault = {
-    "sort_column": "column_name",
+    "sort_column": "Title",
     "path": "path/to/file.csv",
     "sort_order": "asc"
 }
